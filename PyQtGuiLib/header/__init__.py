46985022be9850dabdc30041ee03acefe596e3e2
LX-sys <lx984608061@163.com>
2022-12-11 00:55:20 +0800

    增加PYQT_VERSIONS的倒入方式,修复pyside系列在mac下无法运行的情况

--- PyQtGuiLib/header/__init__.py
+++ PyQtGuiLib/header/__init__.py
@@ -2,3 +2,10 @@ from PyQtGuiLib.header.Qt.qtWidgets import *
 from PyQtGuiLib.header.Qt.qtGui import *
 from PyQtGuiLib.header.Qt.qtCore import *
 from PyQtGuiLib.header.py.common import *
+
+from PyQtGuiLib.header.versions import PYQT_VERSIONS
+
+
+# 处理mac下无法运行的情况
+if sys.platform == "darwin" and PYQT_VERSIONS in ["PySide2","PySide6"]:
+    os.environ["QT_MAC_WANTS_LAYER"] = "1"
