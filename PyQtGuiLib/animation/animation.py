265cc03448a41304df7f7a00419969155e1d5f7f
LX-sys <lx984608061@163.com>
2022-12-12 18:19:13 +0800

    .

---
+++ PyQtGuiLib/animation/animation.py
@@ -0,0 +1,20 @@
+# -*- coding:utf-8 -*-
+# @time:2022/12/1211:41
+# @author:LX
+# @file:animation.py
+# @software:PyCharm
+
+from PyQtGuiLib.header import (
+    sys,
+    QApplication,
+    QPropertyAnimation,
+    QWidget,
+    QPushButton
+)
+
+
+# 动画工厂
+class AnimationFactory:
+    def __init__(self):
+        pass
+
