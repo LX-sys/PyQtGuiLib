21c15b0fc9052888ef76e8c852bad2e40bd60689
LX-sys <lx984608061@163.com>
2022-12-17 18:33:25 +0800

    更新日志

--- PyQtGuiLib/Log/developmentLog.py
+++ PyQtGuiLib/Log/developmentLog.py
@@ -12,5 +12,6 @@
 2022.12.13  0.0.1.4版本  气泡窗口已经基本实现,持续优化更新
 2022.12.13  0.0.1.5版本
     --- 气泡窗口 优化了代码,修复了文字大小无法设置的BUG,以及文字位置的问题
+    --- 新增无边框窗口 BorderlessWidget 类,导入方式 from PyQtGuiLib.core.widgets.borderlessWidget import BorderlessWidget
 
 '''
