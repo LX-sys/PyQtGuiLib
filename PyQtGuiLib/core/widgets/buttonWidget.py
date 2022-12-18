a7287b307772adce6bdfba7eb2e2dca625793b64
LX-sys <lx984608061@163.com>
2022-12-18 22:45:09 +0800

    可点击的窗口

---
+++ PyQtGuiLib/core/widgets/buttonWidget.py
@@ -0,0 +1,35 @@
+from PyQtGuiLib.header import (
+    PYQT_VERSIONS,
+    sys,
+    QApplication,
+    Qt,
+    QMouseEvent,
+    Signal
+)
+
+from PyQtGuiLib.core.widgets import RoundWidget
+
+
+'''
+    可点击的窗口
+'''
+class ButtonWidget(RoundWidget):
+    clicked = Signal()
+
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+
+    def mousePressEvent(self, e:QMouseEvent) -> None:
+        if e.button() == Qt.LeftButton:
+            self.clicked.emit()
+        super().mousePressEvent(e)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    win = ButtonWidget()
+    win.show()
+
+    if PYQT_VERSIONS == "PyQt6":
+        sys.exit(app.exec())
+    else:
+        sys.exit(app.exec_())
