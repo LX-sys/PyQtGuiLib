b23353a29872944a2a8789d6f45978391263bcc0
LX-sys <lx984608061@163.com>
2022-12-18 22:24:09 +0800

    无边框窗口

--- PyQtGuiLib/core/widgets/borderlessWidget.py
+++ PyQtGuiLib/core/widgets/borderlessWidget.py
@@ -10,126 +10,239 @@ from PyQtGuiLib.header import (
     QWidget,
     Qt,
     QMouseEvent,
-    QPoint
+    QPoint,
+    QMainWindow,
+    QFrame,
+    QStackedWidget
 )
 
 '''
     无边框窗口
         可自由拉伸八个方位
+    
+    目前支持的原型窗口
+    QMainWindow
+    QWidget
+    QFrame,
+    QStackedWidget
 '''
 
-class BorderlessWidget(QWidget):
+
+# 无边框窗口移动的提示实现类
+class Borderless:
     Top = "top"
     Down = "Down"
     Left = "Left"
     Right = "Right"
 
     def __init__(self,*args,**kwargs):
-        super().__init__(*args,**kwargs)
-        self.resize(800,500)
-        self.setObjectName("BorderlessWidget")
-        self.setStyleSheet('''
-#BorderlessWidget{
-background-color:green;
-}
-''')
-        self.setWindowFlags(Qt.FramelessWindowHint)
-
-        # 开启鼠标跟踪
-        self.setMouseTracking(True)
-
-        self.scope = 9  # 检测鼠标是否在边缘按下的范围
+        self.scope = 5  # 检测鼠标是否在边缘按下的范围
         self.pressDirection = []  # 记录鼠标点击的边的方向
-        self.pressPos = QPoint(0,0) # 鼠标按下时的位置
+        self.pressPos = QPoint(0, 0)  # 鼠标按下时的位置
         self.pressState = False  # 按下状态
+        self.movePressState = False  # 记录移动时是否时按下的状态
 
     # 边缘检测
-    def isEdge(self,pos:QPoint):
+    def isEdge(self,parent:QWidget,pos:QPoint):
         x,y = pos.x(),pos.y()
-        w,h = self.width(),self.height()
+        w,h = parent.width(),parent.height()
 
         if x <= self.scope and x >= 0:
-            self.pressDirection.append(BorderlessWidget.Left)
+            self.pressDirection.append(Borderless.Left)
         if w - x <= self.scope:
-            self.pressDirection.append(BorderlessWidget.Right)
+            self.pressDirection.append(Borderless.Right)
         if y >= 0 and y <= self.scope:
-            self.pressDirection.append(BorderlessWidget.Top)
+            self.pressDirection.append(Borderless.Top)
         if h - y <= self.scope:
-            self.pressDirection.append(BorderlessWidget.Down)
+            self.pressDirection.append(Borderless.Down)
 
         if self.pressDirection:
             return True
 
     # 更新鼠标的图标
-    def updateCursor(self,pos:QPoint):
+    def updateCursor(self,parent:QWidget, pos: QPoint):
         x, y = pos.x(), pos.y()
-        w, h = self.width(), self.height()
-
-        if (x <= self.scope and x >= 0) or (w - x <= self.scope):
-            self.setCursor(Qt.SizeHorCursor)
+        w, h = parent.width(), parent.height()
+        '''
+            先判断四角,再判断四边
+        '''
+        if (x <= self.scope and x >= 0) and (y >= 0 and y <= self.scope) \
+                or (w - x <= self.scope) and (h - y <= self.scope):
+            parent.setCursor(Qt.SizeFDiagCursor)
+        elif (w - x <= self.scope) and (y >= 0 and y <= self.scope) \
+                or (x <= self.scope and x >= 0) and (h - y <= self.scope):
+            parent.setCursor(Qt.SizeBDiagCursor)
+        elif (x <= self.scope and x >= 0) or (w - x <= self.scope):
+            parent.setCursor(Qt.SizeHorCursor)
         elif (y >= 0 and y <= self.scope) or (h - y <= self.scope):
-            self.setCursor(Qt.SizeVerCursor)
+            parent.setCursor(Qt.SizeVerCursor)
+
         else:
-            self.setCursor(Qt.ArrowCursor)
+            parent.setCursor(Qt.ArrowCursor)
 
         if self.pressDirection:
             return True
 
-    # 扩大边
-    def expandEdge(self,pos:QPoint):
+    # 扩展边
+    def expandEdge(self,parent:QWidget,pos:QPoint):
         if not self.pressState:
             return
 
-        x,y = self.pos().x(),self.pos().y()
+        x,y = parent.pos().x(),parent.pos().y()
         for direction in self.pressDirection:
-            if direction == BorderlessWidget.Right:
+            if direction == Borderless.Right:
                 distance = pos - self.pressPos
-                distance_ = self.width() + distance.x()
-                self.resize(distance_, self.height())
-                if BorderlessWidget.Down not in self.pressDirection:
+                distance_ = parent.width() + distance.x()
+                parent.resize(distance_, parent.height())
+                if Borderless.Down not in self.pressDirection:
                     self.pressPos = QPoint(distance_, 0)
-            if direction == BorderlessWidget.Down:
+            if direction == Borderless.Down:
                 distance = pos - self.pressPos
-                distance_ = self.height() + distance.y()
-                if BorderlessWidget.Right in self.pressDirection:
-                    distance_w = self.width() + distance.x()
-                    self.resize(distance_w, distance_)
+                distance_ = parent.height() + distance.y()
+                if Borderless.Right in self.pressDirection:
+                    distance_w = parent.width() + distance.x()
+                    parent.resize(distance_w, distance_)
                     self.pressPos = QPoint(distance_w, distance_)
                 else:
-                    self.resize(self.width(),distance_)
+                    parent.resize(parent.width(),distance_)
                     self.pressPos = QPoint(0,distance_)
-            if direction == BorderlessWidget.Left:
+            if direction == Borderless.Left:
                 distance_ = x+pos.x()
-                self.resize(self.width()-pos.x(),self.height())
-                self.move(distance_, y)
-            if direction == BorderlessWidget.Top:
+                parent.resize(parent.width()-pos.x(),parent.height())
+                parent.move(distance_, y)
+            if direction == Borderless.Top:
                 distance_ = y + pos.y()
-                self.resize(self.width(), self.height()-pos.y())
-                if BorderlessWidget.Left in self.pressDirection:
+                parent.resize(parent.width(), parent.height()-pos.y())
+                if Borderless.Left in self.pressDirection:
                     x = x+pos.x()
-                self.move(x,distance_)
+                parent.move(x,distance_)
 
-    def mousePressEvent(self, e:QMouseEvent) -> None:
-        if self.isEdge(e.pos()):
+    def pressEvent(self,parent:QWidget, e:QMouseEvent) -> None:
+        if self.isEdge(parent,e.pos()):
             self.pressState = True
             self.pressPos = e.pos()
-        super().mousePressEvent(e)
+        elif e.button() == Qt.LeftButton:  # 处理窗口移动
+            self.movePressState = True
+            self.pressPos = e.globalPos()-parent.pos()
+            parent.setCursor(Qt.OpenHandCursor)
 
-    def mouseReleaseEvent(self, e:QMouseEvent) -> None:
+    def releaseEvent(self) -> None:
         self.pressDirection.clear()
         self.pressPos = QPoint(0, 0)
         self.pressState = False
+        self.movePressState = False
+
+    def moveEvent(self,parent:QWidget, e:QMouseEvent) -> None:
+        if self.movePressState:
+            parent.move(e.globalPos()-self.pressPos)
+        self.updateCursor(parent,e.pos())
+        self.expandEdge(parent,e.pos())
+
+
+# 无边框的主窗口
+class BorderlessMainWindow(QMainWindow):
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        self.resize(800, 500)
+
+        self.borderless = Borderless()
+
+        self.setWindowFlags(Qt.FramelessWindowHint)
+        # 开启鼠标跟踪
+        self.setMouseTracking(True)
+
+    def mousePressEvent(self, e:QMouseEvent) -> None:
+        self.borderless.pressEvent(self,e)
+        super().mousePressEvent(e)
+
+    def mouseReleaseEvent(self, e:QMouseEvent) -> None:
+        self.borderless.releaseEvent()
         super().mouseReleaseEvent(e)
 
     def mouseMoveEvent(self, e:QMouseEvent) -> None:
-        self.updateCursor(e.pos())
-        self.expandEdge(e.pos())
-
+        self.borderless.moveEvent(self,e)
         super().mouseMoveEvent(e)
+
+
+# 无边框的QWidget
+class BorderlessWidget(QWidget):
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        self.resize(800, 500)
+
+        self.borderless = Borderless()
+
+        self.setWindowFlags(Qt.FramelessWindowHint)
+        # 开启鼠标跟踪
+        self.setMouseTracking(True)
+
+    def mousePressEvent(self, e:QMouseEvent) -> None:
+        self.borderless.pressEvent(self,e)
+        super().mousePressEvent(e)
+
+    def mouseReleaseEvent(self, e:QMouseEvent) -> None:
+        self.borderless.releaseEvent()
+        super().mouseReleaseEvent(e)
+
+    def mouseMoveEvent(self, e:QMouseEvent) -> None:
+        self.borderless.moveEvent(self,e)
+        super().mouseMoveEvent(e)
+
+
+# 无边框的QFrame
+class BorderlessFrame(QFrame):
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        self.resize(800, 500)
+
+        self.borderless = Borderless()
+
+        self.setWindowFlags(Qt.FramelessWindowHint)
+        # 开启鼠标跟踪
+        self.setMouseTracking(True)
+
+    def mousePressEvent(self, e:QMouseEvent) -> None:
+        self.borderless.pressEvent(self,e)
+        super().mousePressEvent(e)
+
+    def mouseReleaseEvent(self, e:QMouseEvent) -> None:
+        self.borderless.releaseEvent()
+        super().mouseReleaseEvent(e)
+
+    def mouseMoveEvent(self, e:QMouseEvent) -> None:
+        self.borderless.moveEvent(self,e)
+        super().mouseMoveEvent(e)
+
+
+# 无边框的QStackedWidget
+class BorderlessStackedWidget(QStackedWidget):
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        self.resize(800, 500)
+
+        self.borderless = Borderless()
+
+        self.setWindowFlags(Qt.FramelessWindowHint)
+        # 开启鼠标跟踪
+        self.setMouseTracking(True)
+
+    def mousePressEvent(self, e:QMouseEvent) -> None:
+        self.borderless.pressEvent(self,e)
+        super().mousePressEvent(e)
+
+    def mouseReleaseEvent(self, e:QMouseEvent) -> None:
+        self.borderless.releaseEvent()
+        super().mouseReleaseEvent(e)
+
+    def mouseMoveEvent(self, e:QMouseEvent) -> None:
+        self.borderless.moveEvent(self,e)
+        super().mouseMoveEvent(e)
+
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    win = BorderlessWidget()
+    win = BorderlessStackedWidget()
     win.show()
 
     if PYQT_VERSIONS == "PyQt6":
