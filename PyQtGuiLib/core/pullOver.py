6753bd4bffcc0f91d4c47fdaf2bcab3cf804bb28
LX-sys <lx984608061@163.com>
2022-12-19 11:50:46 +0800

    将pullOverWidget正式更名为pullOver,并将靠边功能给抽象的出来

--- PyQtGuiLib/core/pullOver.py
+++ PyQtGuiLib/core/pullOver.py
@@ -45,6 +45,74 @@ class MonitoThread(QThread):
             self.msleep(self.monito_ms)
 
 
+# 窗口靠边
+class PullOver:
+    def __init__(self,parent:QWidget,*args,**kwargs):
+        self.parent = parent
+        super().__init__(*args,**kwargs)
+        self.move_ani = QPropertyAnimation(self.parent)
+        self.move_ani.setPropertyName(b"pos")
+        self.move_ani.setTargetObject(self.parent)
+        self.move_ani.setEasingCurve(QEasingCurve.OutBounce)
+
+        self.monitoth = MonitoThread(self.parent)
+        self.monitoth.start()
+
+    # 核心方法
+    def pullover(self,btn_obj:QWidget,pos:QPoint=None,small_btn_pos:QPoint=None):
+        '''
+            btn_obj:是一个具有clicked点击事件的按钮,或者其他控件
+            pos:窗口显示的位置,默认居中
+            small_btn_pos:窗口隐藏后按钮显示的位置
+        '''
+        if pos is None:
+            pos = self.center()
+
+        if small_btn_pos is None:
+            small_btn_pos = QPoint(0,300)
+
+        self.showWin(btn_obj,pos)
+        self.hideWin(btn_obj,small_btn_pos)
+
+    def showWin(self,btn_obj:QWidget,pos:QPoint):
+        btn_obj.clicked.connect(lambda: self.show_win_event(btn_obj, pos))
+
+    def hideWin(self,btn_obj:QWidget,small_win_pos:QPoint=None):
+        self.monitoth.winPosed.connect(lambda pos: self.winHide_event(btn_obj, pos,small_win_pos))
+
+    # 靠边隐藏
+    def winHide_event(self,btn_obj:QWidget,pos:QPoint,small_btn_pos:QPoint=None):
+        x,y = pos.x(),pos.y()
+        if x <= 10:
+            self.monitoth.setStop(True)
+            self.move_ani.setStartValue(self.parent.pos())
+            self.move_ani.setEndValue(QPoint(-self.parent.width(),300))
+            self.move_ani.start()
+            def _t(btn,self):
+                btn.hide()
+                btn_obj.move(small_btn_pos)
+                btn_obj.show()
+                self.move_ani.disconnect()
+            # 解开暂停
+            self.move_ani.finished.connect(lambda :_t(self.parent,self))
+
+    def show_win_event(self,btn_obj:QWidget,pos:QPoint):
+        btn_obj.hide()
+        self.parent.show()
+        self.move_ani.setStartValue(self.parent.pos())
+        self.move_ani.setEndValue(pos)
+        self.move_ani.start()
+        def _t(self):
+            self.monitoth.setStop(False)
+            self.move_ani.disconnect()
+        self.move_ani.finished.connect(lambda :_t(self))
+
+    def center(self):
+        f = self.parent.frameGeometry()
+        c = QDesktopWidget().availableGeometry().center()
+        f.moveCenter(c)
+        return f.topLeft()
+
 class PullOverWidget(QWidget):
     def __init__(self,*args,**kwargs):
         super().__init__(*args,**kwargs)
