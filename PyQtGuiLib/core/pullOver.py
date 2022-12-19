3d1758c3bee972c77d08f2df2635a90b6919e2d0
LX-sys <lx984608061@163.com>
2022-12-19 14:55:17 +0800

    靠边窗口已经基本完成(上左右)

--- PyQtGuiLib/core/pullOver.py
+++ PyQtGuiLib/core/pullOver.py
@@ -25,7 +25,7 @@ class MonitoThread(QThread):
         self.widget = widget
         self.stop = False # 暂停
         self.termination_th = False # 终止
-        self.monito_ms = 20  # 检测已毫秒为单位
+        self.monito_ms = 150  # 检测已毫秒为单位
 
     def setTermination(self,b:bool):
         self.termination_th = b
@@ -47,16 +47,24 @@ class MonitoThread(QThread):
 
 # 窗口靠边
 class PullOver:
+    # 弹力
+    OutBounce = QEasingCurve.OutBounce
+
     def __init__(self,parent:QWidget,*args,**kwargs):
         self.parent = parent
         super().__init__(*args,**kwargs)
+
+        self.scope = 10
+
         self.move_ani = QPropertyAnimation(self.parent)
         self.move_ani.setPropertyName(b"pos")
         self.move_ani.setTargetObject(self.parent)
-        self.move_ani.setEasingCurve(QEasingCurve.OutBounce)
-
         self.monitoth = MonitoThread(self.parent)
         self.monitoth.start()
+
+    # 设置动效
+    def setEasingCurve(self,easing):
+        self.move_ani.setEasingCurve(easing)
 
     # 核心方法
     def pullover(self,btn_obj:QWidget,pos:QPoint=None,small_btn_pos:QPoint=None):
@@ -67,9 +75,6 @@ class PullOver:
         '''
         if pos is None:
             pos = self.center()
-
-        if small_btn_pos is None:
-            small_btn_pos = QPoint(0,300)
 
         self.showWin(btn_obj,pos)
         self.hideWin(btn_obj,small_btn_pos)
@@ -83,16 +88,48 @@ class PullOver:
     # 靠边隐藏
     def winHide_event(self,btn_obj:QWidget,pos:QPoint,small_btn_pos:QPoint=None):
         x,y = pos.x(),pos.y()
-        if x <= 10:
+        desktop_w = QApplication.desktop().width()
+        desktop_h = QApplication.desktop().height()
+
+        def _t(parent, self):
+            parent.hide()
+            btn_obj.move(small_btn_pos)
+            btn_obj.show()
+            self.move_ani.disconnect()
+
+        # Window current position
+        pos_ = self.parent.pos()
+        px, py = pos.x(), pos_.y()
+
+        if x <= self.scope:
+            if small_btn_pos is None:
+                small_btn_pos = QPoint(0, desktop_h>>1)
+                # small_btn_pos = QPoint(0, py)
+
+            self.monitoth.setStop(True)
+            self.move_ani.setStartValue(pos_)
+            self.move_ani.setEndValue(QPoint(-self.parent.width(),py))
+            self.move_ani.start()
+            # 解开暂停
+            self.move_ani.finished.connect(lambda :_t(self.parent,self))
+        elif desktop_w - (x + self.parent.width()) <= self.scope:  # right
+            if small_btn_pos is None:
+                small_btn_pos = QPoint(desktop_w - btn_obj.width(), desktop_h>>1)
+
             self.monitoth.setStop(True)
             self.move_ani.setStartValue(self.parent.pos())
-            self.move_ani.setEndValue(QPoint(-self.parent.width(),300))
+            self.move_ani.setEndValue(QPoint(desktop_w+self.parent.width(), py))
             self.move_ani.start()
-            def _t(btn,self):
-                btn.hide()
-                btn_obj.move(small_btn_pos)
-                btn_obj.show()
-                self.move_ani.disconnect()
+            # 解开暂停
+            self.move_ani.finished.connect(lambda: _t(self.parent, self))
+        elif y <= self.scope:
+            if small_btn_pos is None:
+                small_btn_pos = QPoint(desktop_w>>1,0)
+
+            self.monitoth.setStop(True)
+            self.move_ani.setStartValue(self.parent.pos())
+            self.move_ani.setEndValue(QPoint(px,-self.parent.height()))
+            self.move_ani.start()
             # 解开暂停
             self.move_ani.finished.connect(lambda :_t(self.parent,self))
 
@@ -113,76 +150,9 @@ class PullOver:
         f.moveCenter(c)
         return f.topLeft()
 
-class PullOverWidget(QWidget):
-    def __init__(self,*args,**kwargs):
-        super().__init__(*args,**kwargs)
-        self.resize(500,500)
-
-        self.show_btn = ButtonWidget()
-        self.show_btn.resize(70, 70)
-        self.show_btn.setObjectName("show_btn")
-        self.show_btn.setStyleSheet('''
-        #show_btn{
-        background-color:green;
-        border-radius:35px;
-        }
-        ''')
-        self.show_btn.clicked.connect(self.show_win_event)
-        # 动画
-        self.move_ani = QPropertyAnimation(self)
-        self.move_ani.setPropertyName(b"pos")
-        self.move_ani.setTargetObject(self)
-        self.move_ani.setEasingCurve(QEasingCurve.OutBounce)
-
-        self.monitoth = MonitoThread(self)
-        self.monitoth.start()
-        self.monitoth.winPosed.connect(self.winHide_event)
-
-    # 靠边隐藏
-    def winHide_event(self,pos:QPoint):
-        x,y = pos.x(),pos.y()
-        if x <= 10:
-            self.monitoth.setStop(True)
-            self.move_ani.setStartValue(self.pos())
-            self.move_ani.setEndValue(QPoint(-self.width(),300))
-            self.move_ani.start()
-            def _(self):
-                self.hide()
-                self.show_btn.move(0,300)
-                self.show_btn.show()
-                self.move_ani.disconnect()
-            # 解开暂停
-            self.move_ani.finished.connect(lambda :_(self))
-
-    def show_win_event(self):
-        self.show_btn.hide()
-        self.show()
-        self.move_ani.setStartValue(self.pos())
-        self.move_ani.setEndValue(self.center())
-        self.move_ani.start()
-        def _(self):
-            self.monitoth.setStop(False)
-            self.move_ani.disconnect()
-        self.move_ani.finished.connect(lambda :_(self))
-
-    def center(self):
-        f = self.frameGeometry()
-        c = QDesktopWidget().availableGeometry().center()
-        f.moveCenter(c)
-        return f.topLeft()
-
-    def closeEvent(self, event:QCloseEvent) -> None:
+    '''
+        请在你的窗口中,重写关闭事件,并加入这一句话
+    '''
+    def closeThread(self):
         # 在关闭之前,先终止线程
         self.monitoth.setTermination(True)
-        super().closeEvent(event)
-
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    win = PullOverWidget()
-    win.show()
-
-    if PYQT_VERSIONS == "PyQt6":
-        sys.exit(app.exec())
-    else:
-        sys.exit(app.exec_())
