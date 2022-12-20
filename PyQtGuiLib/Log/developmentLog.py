a2d5f161ca67ae020538f4682705c851d63fd8ea
LX-sys <lx984608061@163.com>
2022-12-20 15:49:28 +0800

    更新日志

--- PyQtGuiLib/Log/developmentLog.py
+++ PyQtGuiLib/Log/developmentLog.py
@@ -11,10 +11,12 @@
 '''
 2022.12.13  0.0.1.4版本  气泡窗口已经基本实现,持续优化更新
 2022.12.13  0.0.1.5版本  重写了气泡窗口的部分实现代码
-2022.12.13  0.0.1.6版本
+2022.12.19  0.0.2.0版本
     --- 气泡窗口 优化了代码,修复了文字大小无法设置的BUG,以及文字位置的问题
     --- 新增无边框窗口 BorderlessWidget 类,导入方式 from PyQtGuiLib.core.widgets import BorderlessWidget
     --- 新增圆角窗口 RoundWidget 类,导入方式 from PyQtGuiLib.core.widgets import RoundWidget
-    --- 靠边窗口功能完成,PullOver类 导入方式 from PyQtGuiLib.core.pullOver import PullOver
+    --- 靠边窗口功能完成(任意窗口都可以加上该功能),PullOver类 导入方式 from PyQtGuiLib.core.pullOver import PullOver
     --- 靠边窗口功能的测试用例已编写
+2022.12.20  0.0.3.0版本
+    --- 新增圆环功能
 '''
