86f783cb75f0ae86bd0ec8bd983e3ee2cceded33
LX-sys <lx984608061@163.com>
2022-12-21 12:33:42 +0800

    亚克力效果不良好

--- PyQtGuiLib/core/widgets/acrylic/acrylicWidget.py
+++ PyQtGuiLib/core/widgets/acrylic/acrylicWidget.py
@@ -14,24 +14,24 @@ from PyQtGuiLib.header import (
 
 from windowEffect import WindowEffect
 
-class AcrylicWidget(QWidget):
+from PyQtGuiLib.core.widgets import BorderlessWidget
+
+class AcrylicWidget(BorderlessWidget):
     def __init__(self,*args,**kwargs):
         super().__init__(*args,**kwargs)
         self.resize(800,600)
 
         self.setWindowFlags(Qt.FramelessWindowHint)  # make the window frameless
         self.setAttribute(Qt.WA_TranslucentBackground)  # make the window translucent
-
+        #
         self.ui_layout = QGridLayout(self)  # create a ui layout
         self.ui_layout.setAlignment(Qt.AlignCenter)  # center the layout
 
         self.label = QLabel("Hello World!", self)  # create a label to display a text
         self.label.setFont(QFont("Segoe UI", 14))  # configure the text size and font
         self.ui_layout.addWidget(self.label)  # add the label widget into the layout
-
         self.windowFX = WindowEffect()  # instatiate the WindowEffect class
         self.windowFX.setAcrylicEffect(self.winId())  # set the Acrylic effect by
-
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
