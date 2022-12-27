90a727d73f4cc9dbf8f53d499f718a2d0fa3564d
LX-sys <lx984608061@163.com>
2022-12-27 10:20:23 +0800

    新增两个方法,setAnimationTime(),setAinDirectionMode()

--- PyQtGuiLib/core/slideShow.py
+++ PyQtGuiLib/core/slideShow.py
@@ -29,9 +29,13 @@ class SlideShow(QWidget):
 
     # 扁平 风格
     FlatMode = "flat"  # 经典
-    # 方向
-    Left = "left"
-    Right = "right"
+
+    # 动画的方向模式
+    Ani_Left = "left"
+    Ani_Right = "right"
+    Ani_Down = "down"
+    Ani_Up = "up"
+
 
     # 动画
     Translation = "translation"  # 平移
@@ -48,19 +52,19 @@ class SlideShow(QWidget):
         self.index = [-1,0]  # 索引
         self.cu_index = self.index[1]  # 当前索引
 
-        # 设置自动轮播,方向
+        # 设置自动轮播,自动轮播的方向
         self.is_auto_slide = False
-        self.direction = SlideShow.Right
+        self.auto_direction = SlideShow.Ani_Right
 
         # 左右按钮
-        self.left_btn = QPushButton("左",self)
-        self.right_btn = QPushButton("右",self)
-        self.left_btn.setObjectName("left_btn")
-        self.right_btn.setObjectName("right_btn")
-        self.left_btn.resize(50,50)
-        self.right_btn.resize(50,50)
-        self.left_btn.clicked.connect(lambda :self.roll_event(SlideShow.Left))
-        self.right_btn.clicked.connect(lambda :self.roll_event(SlideShow.Right))
+        self.Ani_Left_btn = QPushButton("左",self)
+        self.Ani_Right_btn = QPushButton("右",self)
+        self.Ani_Left_btn.setObjectName("Ani_Left_btn")
+        self.Ani_Right_btn.setObjectName("Ani_Right_btn")
+        self.Ani_Left_btn.resize(50,50)
+        self.Ani_Right_btn.resize(50,50)
+        self.Ani_Left_btn.clicked.connect(lambda :self.roll_event(SlideShow.Ani_Left))
+        self.Ani_Right_btn.clicked.connect(lambda :self.roll_event(SlideShow.Ani_Right))
         self.btnStyle()
 
         # 窗口
@@ -80,9 +84,21 @@ class SlideShow(QWidget):
 background-color:gray;
 border-radius:5px;
             ''')
+    
+    # 设置动画方向模式
+    def setAinDirectionMode(self,modes:tuple):
+        if isinstance(modes,tuple):
+            self.Ani_Left_btn.disconnect()
+            self.Ani_Right_btn.disconnect()
+            self.Ani_Left_btn.clicked.connect(lambda: self.roll_event(modes[0]))
+            self.Ani_Right_btn.clicked.connect(lambda: self.roll_event(modes[1]))
+
+    # 设置动画间隔时间
+    def setAnimationTime(self,interval:int=300):
+        self.animation_time = interval
 
     # 自动轮播
-    def setAutoSlideShow(self,b:bool,interval:int=2000,direction: str="right",is_not_show_btn:bool=True):
+    def setAutoSlideShow(self,b:bool,interval:int=2000,direction: str="Ani_Right",is_not_show_btn:bool=True):
         '''
             b: 是否启动自动轮播
             interval:切换窗口的事件间隔
@@ -95,18 +111,18 @@ border-radius:5px;
 
         self.setHideButtons(is_not_show_btn)
 
-        self.direction = direction
-        self.left_btn.setEnabled(not b)
-        self.right_btn.setEnabled(not b)
+        self.auto_direction = direction
+        self.Ani_Left_btn.setEnabled(not b)
+        self.Ani_Right_btn.setEnabled(not b)
 
     # 设置隐藏/显示左右按钮
     def setHideButtons(self,b:bool=False):
         if b:
-            self.left_btn.hide()
-            self.right_btn.hide()
+            self.Ani_Left_btn.hide()
+            self.Ani_Right_btn.hide()
         else:
-            self.left_btn.show()
-            self.right_btn.show()
+            self.Ani_Left_btn.show()
+            self.Ani_Right_btn.show()
 
     # 获取当前窗口的数量
     def getWidgetCount(self) -> int:
@@ -167,14 +183,12 @@ border-radius:5px;
             obj_or_index.hide()
             self.widgets.remove(obj_or_index)
 
-
     # 切换到指定窗口(暂时放弃改函数)
-    # def setCurrentIndex(self,index:int=0,is_animation:bool=False):
-    #     pass
-        # if not is_animation:
-        #     widget = self.getWidget(index)
-        #
-        #     self.__createWidget(widget)
+    def setCurrentIndex(self,index:int=0,is_animation:bool=False):
+        if not is_animation:
+            widget = self.getWidget(index)
+
+            self.__createWidget(widget)
 
     # 获取当前索引
     def getIndex(self)->int:
@@ -185,46 +199,46 @@ border-radius:5px;
 
     # 切换到下一个窗口
     def next(self):
-        self.roll_event(SlideShow.Right)
+        self.roll_event(SlideShow.Ani_Right)
 
     # 切换到上一个窗口
-    def up(self):
-        self.roll_event(SlideShow.Left)
+    def Ani_Up(self):
+        self.roll_event(SlideShow.Ani_Left)
 
     # 两侧的按钮
     def btnPos(self):
-        self.left_btn.move(5, self.height() // 2 - self.left_btn.height() // 2)
-        self.right_btn.move(self.width() - self.right_btn.width() - 5,
-                            self.height() // 2 - self.left_btn.height() // 2)
+        self.Ani_Left_btn.move(5, self.height() // 2 - self.Ani_Left_btn.height() // 2)
+        self.Ani_Right_btn.move(self.width() - self.Ani_Right_btn.width() - 5,
+                            self.height() // 2 - self.Ani_Left_btn.height() // 2)
 
     # 按钮样式
     def btnStyle(self):
-        self.left_btn.setStyleSheet('''
-#left_btn{
+        self.Ani_Left_btn.setStyleSheet('''
+#Ani_Left_btn{
 background-color:transparent;
 border:2px solid #73ffcc;
 color:#73ffcc;
 border-radius:5px;
 }
-#left_btn:hover{
+#Ani_Left_btn:hover{
 border:2px solid #00557f;
 }
         ''')
-        self.right_btn.setStyleSheet('''
-#right_btn{
+        self.Ani_Right_btn.setStyleSheet('''
+#Ani_Right_btn{
 background-color:transparent;
 border:2px solid #73ffcc;
 color:#73ffcc;
 border-radius:5px;
 }
-#right_btn:hover{
+#Ani_Right_btn:hover{
 border:2px solid #00557f;
 }
                 ''')
 
     # 返回左右按钮对象
     def getButtons(self) -> tuple:
-        return self.left_btn,self.right_btn
+        return self.Ani_Left_btn,self.Ani_Right_btn
 
     # 展示
     def show_(self):
@@ -262,13 +276,20 @@ border:2px solid #00557f;
                 move_ani2.setTargetObject(widget2)
 
                 move_ani.setStartValue(widget.pos())
-                if direction == SlideShow.Right:
+                if direction == SlideShow.Ani_Right:
                     end_value,start_value = QPoint(widget.width(), 0),QPoint(-widget.width(), 0)
-                elif direction == SlideShow.Left:
+                elif direction == SlideShow.Ani_Left:
                     end_value,start_value = QPoint(-widget.width(), 0),QPoint(widget.width(), 0)
+                elif direction == SlideShow.Ani_Down:
+                    end_value,start_value = QPoint(0,widget.height()),QPoint(0,-widget.height())
+                elif direction == SlideShow.Ani_Up:
+                    end_value, start_value = QPoint(0, -widget.height()), QPoint(0, widget.height())
+                else:
+                    end_value, start_value = QPoint(widget.width(), 0), QPoint(-widget.width(), 0)
                 move_ani.setEndValue(end_value)
                 move_ani2.setStartValue(start_value)
                 move_ani2.setEndValue(QPoint(0, 0))
+
 
                 move_ani.setDuration(self.animation_time)
                 move_ani2.setDuration(self.animation_time)
@@ -283,7 +304,7 @@ border:2px solid #00557f;
 
     def timerEvent(self, e) -> None:
         if self.is_auto_slide:
-            self.roll_event(self.direction)
+            self.roll_event(self.auto_direction)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
