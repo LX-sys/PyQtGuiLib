e23865f9a5330e41d8f725777724f967237b59d1
LX-sys <lx984608061@163.com>
2022-12-27 14:48:49 +0800

    更新

--- PyQtGuiLib/core/progressBar/__init__.py
+++ PyQtGuiLib/core/progressBar/__init__.py
@@ -2,3 +2,4 @@ from PyQtGuiLib.core.progressBar.circularBar import CircularBar
 from PyQtGuiLib.core.progressBar.nodeBar import NodeBar
 from PyQtGuiLib.core.progressBar.loadBar import LoadBar
 from PyQtGuiLib.core.progressBar.waterBar import WaterBar
+from PyQtGuiLib.core.progressBar.gradientBar import GradientBar
