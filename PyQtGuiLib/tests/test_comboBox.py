b948d71391ab1dd701080124907f37589b8fda56
LX-sys <lx984608061@163.com>
2023-01-02 18:53:20 -1200

    更新测试

--- PyQtGuiLib/tests/test_comboBox.py
+++ PyQtGuiLib/tests/test_comboBox.py
@@ -22,12 +22,6 @@ from PyQtGuiLib.core.comboBox import ComboBox
     下拉框 测试用例
 '''
 
-class TPushButton(QPushButton):
-    def __init__(self,*args,**kwargs):
-        super().__init__(*args,**kwargs)
-        self.setWindowFlags(Qt.WindowStaysOnTopHint| Qt.WindowTransparentForInput)
-
-
 class TestComboBox(QMainWindow):
     sed = Signal()
     def __init__(self,*args,**kwargs):
@@ -40,30 +34,12 @@ class TestComboBox(QMainWindow):
         self.combox.move(100,100)
         print(self.combox.pos())
 
-
-        self.btn1 = QPushButton("test2",self)
-        self.btn = TPushButton("test", self)
-        self.btn.move(200,0)
-        self.btn.clicked.connect(lambda :print("111"))
-
-        self.btn1.clicked.connect(self.test)
-
     def test(self,e):
         print(e)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     win = TestComboBox()
-
-    c = Signal(int)
-
-    # print(c.signatures)
-    # c.__get__(win,object())
-    setattr(TestComboBox,"ked",c)
-    print(TestComboBox.__dict__)
-    # TestComboBox.ked.signatures = TestComboBox.property(lambda win: object(), lambda win, v: None, lambda win: None)
-    # print(TestComboBox.sed.signatures)
-    # print(TestComboBox.ked.signatures)
     win.show()
 
     if PYQT_VERSIONS == "PyQt6":
