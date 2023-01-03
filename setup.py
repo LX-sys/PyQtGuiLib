d1d6cbf79cd700a2dd3b8fa60cfc16a2dcfc2871
LX-sys <lx984608061@163.com>
2023-01-04 00:12:25 +0800

    更新版本

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import find_packages
 setup(
     name="PyQtGuiLib",
     packages =find_packages(),
-    version="0.0.7.0",
+    version="0.0.8.0",
     author="LX",
     author_email = "lx984608061@163.com",
     description = "Python version of the qt component library.",
