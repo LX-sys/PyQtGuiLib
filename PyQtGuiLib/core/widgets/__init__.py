d60346ff1531d70d2876b57afb9e9ef0ece45c01
LX-sys <lx984608061@163.com>
2023-01-03 17:25:10 -1200

    .

--- PyQtGuiLib/core/widgets/__init__.py
+++ PyQtGuiLib/core/widgets/__init__.py
@@ -13,3 +13,4 @@ from PyQtGuiLib.core.widgets.borderlessWidget import (
 from PyQtGuiLib.core.widgets.roundWidget import RoundWidget
 from PyQtGuiLib.core.widgets.buttonWidget import ButtonWidget
 from PyQtGuiLib.core.widgets.titleBar import TitleBar
+from PyQtGuiLib.core.widgets.statusBar import StatusBar
