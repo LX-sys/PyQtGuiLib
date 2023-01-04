6b2804cb1af3e80f304fed52ca2550a11931bc4a
LX-sys <lx984608061@163.com>
2023-01-05 00:38:58 +0800

    状态栏 基本完成 测试使用中

--- PyQtGuiLib/core/widgets/statusBar.py
+++ PyQtGuiLib/core/widgets/statusBar.py
@@ -5,23 +5,43 @@
 # @software:PyCharm
 
 from PyQtGuiLib.header import (
-    PYQT_VERSIONS,
-    sys,
+    is_win_sys,
+    is_mac_sys,
     time,
-    QApplication,
+    QThread,
+    Signal,
     QWidget,
     QFrame,
     QHBoxLayout,
     QLabel,
     QPushButton,
-    QFont,
-    QFontMetricsF,
     QSpacerItem,
     QSizePolicy,
 )
 '''
     状态栏
 '''
+
+
+# 倒计时类
+class CountDownThread(QThread):
+    # 计时完成信号
+    timeOuted = Signal()
+
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        self.time_ = 0
+
+    # 设置时间
+    def setTime(self,time:int):
+        self.time_ = time
+
+    def run(self) -> None:
+        while self.time_:
+            self.sleep(1)
+            self.time_-=1
+        self.timeOuted.emit()
+
 
 class StatusBar(QFrame):
     def __init__(self,*args,**kwargs):
@@ -39,15 +59,25 @@ class StatusBar(QFrame):
         self.local_time = time.strftime(self.format, time.localtime())
         self.l_time = QLabel(self.local_time)
 
+        # 创建倒计时类
+        self.cd = CountDownThread()
+
         self.__hlay = QHBoxLayout(self)
         self.__hlay.setContentsMargins(6,0,6,0)
-        self.__hlay.setSpacing(6)
+        if is_win_sys:
+            self.__hlay.setSpacing(6)
+        if is_mac_sys:
+            self.__hlay.setSpacing(6*3)
 
         self.hSpacer = QSpacerItem(704, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
 
         self.updateStatusSize()
 
         self.startTimer(1000)
+
+    # 设置时间格式
+    def setTimeFormat(self,format:str="%Y-%m-%d %H:%M:%S"):
+        self.format = format
 
     def setParent(self, parent:QWidget) -> None:
         self.__parent = parent
@@ -63,18 +93,36 @@ class StatusBar(QFrame):
         self.__hlay.addItem(self.hSpacer)
 
     # 添加文本
-    def addText(self,text:str,style:str=None):
+    def addText(self,text:str,style:str=None,duration:int=0):
+        '''
+            text:文本
+            style:样式
+            duration:持续时间后消失
+        '''
         l = QLabel(text)
         if style:
             l.setStyleSheet(style)
+
         self.__hlay.addWidget(l)
         self.__addSpacer()
 
+        # 移除布局
+        def _del():
+            self.__hlay.removeWidget(l)
+            l.deleteLater()  # 销毁自己
+
+        if duration > 0:
+            self.cd.setTime(duration)
+            self.cd.timeOuted.connect(lambda :_del())
+            self.cd.start()
+
     # 添加按钮
-    def addButton(self,text:str,style:str=None):
+    def addButton(self,text:str,style:str=None,callback=None):
         btn = QPushButton(text)
         if style:
             btn.setStyleSheet(style)
+        if callback:
+            btn.clicked.connect(callback)
         self.__hlay.addWidget(btn)
         self.__addSpacer()
 
