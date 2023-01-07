c4e92e9810ada66b3f183f61a8d8b8044697eeec
LX-sys <lx984608061@163.com>
2023-01-07 19:19:59 +0800

    .

--- PyQtGuiLib/core/__init__.py
+++ PyQtGuiLib/core/__init__.py
@@ -1,3 +1,4 @@
 from PyQtGuiLib.core.bubbleWidget import BubbleWidget
 from PyQtGuiLib.core.pullOver import PullOver
 from PyQtGuiLib.core.slideShow import SlideShow
+from PyQtGuiLib.core.flowLayout import FlowLayout
