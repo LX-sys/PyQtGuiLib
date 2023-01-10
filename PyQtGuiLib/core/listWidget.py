c3280de7c2dfa2d2866b8921916c9cea63dcb466
LX-sys <lx984608061@163.com>
2023-01-11 01:40:02 +0800

    QListWidget 增强版本 - ListWidget

--- PyQtGuiLib/core/listWidget.py
+++ PyQtGuiLib/core/listWidget.py
@@ -1,30 +1,16 @@
 from PyQtGuiLib.header import (
-    PYQT_VERSIONS,
-    sys,
-    QApplication,
     QListWidget,
     QListWidgetItem,
     QWidget,
     QSize,
-    QStyledItemDelegate,
-    QStyleOptionViewItem,
-    QModelIndex
+    QPropertyAnimation,
+    QPoint,
+    qt
 )
 
-from PyQtGuiLib.core.resolver import dumpStructure
-
 '''
-
-    QListWidget 增强版本
+    QListWidget 增强版本 - ListWidget
 '''
-
-class ItemDelegate(QStyledItemDelegate):
-    def __init__(self,*args,**kwargs):
-        super().__init__(*args,**kwargs)
-
-    def updateEditorGeometry(self, editor:QWidget, option:QStyleOptionViewItem, index:QModelIndex) -> None:
-        editor.setGeometry(option.rect)
-
 
 class ListWidgetItem(QListWidgetItem):
     def __init__(self,*args,**kwargs):
@@ -32,6 +18,11 @@ class ListWidgetItem(QListWidgetItem):
 
 
 class ListWidget(QListWidget):
+    OutBounce = qt.OutBounce
+    CosineCurve = qt.CosineCurve
+    SineCurve = qt.SineCurve
+
+
     def __init__(self,*args,**kwargs):
         super().__init__(*args,**kwargs)
 
@@ -40,14 +31,29 @@ class ListWidget(QListWidget):
 
         # item的最小高度
         self.item_min_height = 30
+
+        # 动画是否启用,持续时间,动画特效
+        self.ani_enabled = True
+        self.ani_duration = 300
+        self.ani_special = ListWidget.OutBounce
+
         self.defaultStyle()
+
+    # 设置动画是否启用
+    def setAniEnabled(self,b:bool):
+        self.ani_enabled = b
+
+    # 设置持续时间
+    def setAniDuration(self, duration:int):
+        self.ani_duration = duration
+
+    # 设置动画特效
+    def setAniSpecial(self,special):
+        self.ani_special = special
 
     # 设置item的最小高度
     def setItemMinHeight(self,h:int):
         self.item_min_height = h
-
-    def addItem(self, aitem: QListWidgetItem) -> None:
-        super().addItem(aitem)
 
     # 添加 QWidget
     def addWidget(self,widget:QWidget):
@@ -55,10 +61,36 @@ class ListWidget(QListWidget):
 
         item = ListWidgetItem()
         item.setSizeHint(QSize(self.width()-4,widget.height()))
-        self.addItem(item)
-        self.setItemWidget(item, widget)
+        self.widgets.append((item, widget))
 
-        self.widgets.append((item,widget))
+        def _t(self,item,widget):
+            self.addItem(item)
+            self.setItemWidget(item, widget)
+            if self.ani_enabled:
+                temp_ani_widget.deleteLater()
+
+        if self.ani_enabled:
+            # ----------动画
+            # 先创建临时动画窗口
+            temp_ani_widget = QWidget(self)
+            temp_ani_widget.resize(item.sizeHint())
+            temp_ani_widget.setStyleSheet(widget.styleSheet())
+            temp_ani_widget.move(0,self.height()-temp_ani_widget.height()-1)
+            temp_ani_widget.show()
+
+            ani = QPropertyAnimation(temp_ani_widget)
+            ani.setTargetObject(temp_ani_widget)
+            ani.setPropertyName(b"pos")
+            ani.setStartValue(temp_ani_widget.pos())
+            ani.setEndValue(QPoint(0,(len(self.widgets)-1)*self.item_min_height))
+            ani.setDuration(300)
+            ani.setEasingCurve(self.ani_special)
+            ani.start()
+
+            ani.finished.connect(lambda :_t(self,item,widget))
+        else:
+            _t(self,item,widget)
+        # ----------动画
 
     # 默认样式
     def defaultStyle(self):
@@ -87,8 +119,27 @@ width:12px;
             item,wid = ws
             if wid == widget:
                 self.widgets.remove(ws)
-                self.removeItemWidget(item)
-                wid.deleteLater()
-                self.takeItem(i)
-                del item
+
+                def _t(self,item,wid,i):
+                    self.removeItemWidget(item)
+                    wid.deleteLater()
+                    self.takeItem(i)
+                    del item
+
+                if self.ani_enabled:
+                    # -------------
+                    # 移除动画
+                    ani = QPropertyAnimation(wid)
+                    ani.setTargetObject(wid)
+                    ani.setPropertyName(b"pos")
+                    ani.setStartValue(wid.pos())
+                    ani.setEndValue(QPoint(0, self.height()-self.item_min_height))
+                    ani.setDuration(300)
+                    ani.setEasingCurve(self.ani_special)
+                    ani.start()
+
+                    ani.finished.connect(lambda :_t(self,item,wid,i))
+                    # -------------
+                else:
+                    _t(self, item, wid, i)
                 break
