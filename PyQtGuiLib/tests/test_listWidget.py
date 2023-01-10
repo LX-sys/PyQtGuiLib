29830fa3779d7e841d60bda45973cb6cba9e6291
LX-sys <lx984608061@163.com>
2023-01-11 01:40:10 +0800

    QListWidget 增强版本 - ListWidget 测试用例

--- PyQtGuiLib/tests/test_listWidget.py
+++ PyQtGuiLib/tests/test_listWidget.py
@@ -3,14 +3,14 @@ from PyQtGuiLib.header import (
     sys,
     QApplication,
     QWidget,
-    QThread,
-    Signal,
-    QColor,
-    QSlider,
-    QListWidgetItem,
     QLabel,
-    qt
+    qt,
+    QPushButton,
+    uuid
 )
+'''
+    QListWidget 增强版本-ListWidget 测试用例
+'''
 
 from PyQtGuiLib.core import ListWidget
 from PyQtGuiLib.styles import ButtonStyle
@@ -21,21 +21,36 @@ class TestListWidget(QWidget):
         super().__init__(*args,**kwargs)
         self.resize(800,700)
 
+        self.btn = QPushButton("动态添加",self)
+        self.btn.move(10,10)
+        self.btn.clicked.connect(self.addLabel)
+
+        self.delt = QPushButton("动态删除", self)
+        self.delt.move(180, 10)
+        self.delt.clicked.connect(self.delLabel)
+
+        # 增强版本-ListWidget
         self.listw = ListWidget(self)
+        # self.listw.setAniEnabled(False) #  取消动画
         self.listw.resize(600,500)
         self.listw.move(100,100)
         self.listw.itemDoubleClicked.connect(lambda :print("asd"))
 
-        for i in range(30):
+        for i in range(5):
             widget = QLabel(str(i))
             widget.setAlignment(qt.AlignCenter)
-            widget.setStyleSheet(ButtonStyle.randomStyle())
+            widget.setStyleSheet(ButtonStyle.contrastStyle())
             self.listw.addWidget(widget)
 
-        wid = self.listw.getAllWidget()[1]
-        print(wid)
-        # self.listw.removeItemWidget(self.listw.widgets[0][0])
-        # self.listw.adjustSize()
+    def addLabel(self):
+        l = QLabel(str(uuid.uuid1()))
+        l.setAlignment(qt.AlignCenter)
+        l.setStyleSheet(ButtonStyle.contrastStyle())
+        self.listw.addWidget(l)
+
+
+    def delLabel(self):
+        wid = self.listw.getAllWidget()[2]
         self.listw.removeWidget(wid)
 
 if __name__ == '__main__':
