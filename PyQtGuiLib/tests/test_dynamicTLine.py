20f090c287c68a5fecf8e2ce07b10a4fe24d4989
LX-sys <lx984608061@163.com>
2023-01-12 22:59:52 +0800

    动态 标题输入框 测试用例

---
+++ PyQtGuiLib/tests/test_dynamicTLine.py
@@ -0,0 +1,34 @@
+from PyQtGuiLib.header import (
+    PYQT_VERSIONS,
+    QApplication,
+    sys,
+    QWidget
+)
+
+from PyQtGuiLib.core.lineedit import DynamicTLine
+
+'''
+    动态 标题输入框 测试用例
+'''
+
+class Test_DynamicTLine(QWidget):
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        self.resize(600,600)
+
+        self.line = DynamicTLine(self)
+        self.line.move(100,100)
+
+        self.line2 = DynamicTLine(self)
+        self.line2.move(100,300)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    win = Test_DynamicTLine()
+    win.show()
+
+    if PYQT_VERSIONS in ["PyQt6","PySide6"]:
+        sys.exit(app.exec())
+    else:
+        sys.exit(app.exec_())
