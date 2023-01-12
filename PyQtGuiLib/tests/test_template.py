5c5522d29a21bbf834cb5b53489ce0e7aa05c671
LX-sys <lx984608061@163.com>
2023-01-12 18:56:11 +0800

    测试用例的标准模板,该代码用于复制

---
+++ PyQtGuiLib/tests/test_template.py
@@ -0,0 +1,28 @@
+from PyQtGuiLib.header import (
+    PYQT_VERSIONS,
+    QApplication,
+    sys,
+    QWidget
+)
+
+'''
+    测试用例的标准模板,该代码用于复制
+'''
+
+class Test(QWidget):
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        self.resize(600,600)
+
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    win = Test()
+    win.show()
+
+    if PYQT_VERSIONS in ["PyQt6","PySide6"]:
+        sys.exit(app.exec())
+    else:
+        sys.exit(app.exec_())
