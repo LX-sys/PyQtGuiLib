f9084df8b687df26123358b46615e9e9cf2f7d04
LX-sys <lx984608061@163.com>
2023-02-10 09:35:24 +0800

    新增异常类

---
+++ PyQtGuiLib/header/error.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+# @time:2023/2/109:28
+# @author:LX
+# @file:error.py
+# @software:PyCharm
+
+# -----
+
+
+class SyntaxError(Exception):
+    '''
+        语法异常
+    '''
+
+class KeyError(Exception):
+    '''
+        字典的 Key 错误
+    '''
