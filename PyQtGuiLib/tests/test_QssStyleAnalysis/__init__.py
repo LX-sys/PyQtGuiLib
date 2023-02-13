a7d86b529dbd452f914854efeeefe912410579fd
LX-sys <lx984608061@163.com>
2023-02-13 08:34:08 +0800

    finish_event里面还没有写完

--- PyQtGuiLib/tests/test_QssStyleAnalysis/__init__.py
+++ PyQtGuiLib/tests/test_QssStyleAnalysis/__init__.py
@@ -3,3 +3,59 @@
 # @author:LX
 # @file:__init__.py.py
 # @software:PyCharm
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QPushButton, QVBoxLayout, QWidget
+from PyQt5.QtMultimedia import QCamera, QCameraImageCapture
+# QCameraViewfinder
+
+class LoginWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        # 创建窗口
+        self.setWindowTitle('QQ Login')
+        self.setGeometry(100, 100, 300, 200)
+
+        # 创建界面
+        widget = QWidget()
+        self.setCentralWidget(widget)
+
+        # 添加元素
+        label = QLabel('Username:')
+        self.username = QLineEdit()
+        button = QPushButton('Scan to Login')
+        button.clicked.connect(self.scan_login)
+
+        # 布局管理
+        layout = QVBoxLayout()
+        layout.addWidget(label)
+
+        layout.addWidget(self.username)
+        layout.addWidget(button)
+        widget.setLayout(layout)
+
+    def scan_login(self):
+        camera = QCamera()
+        # viewfinder = QCameraViewfinder()
+        image_capture = QCameraImageCapture(camera)
+
+        # camera.setViewfinder(viewfinder)
+        camera.start()
+
+        # 读取二维码信息并完成扫描登录
+        # 此处需要根据具体实现方法进行修改
+        qr_code = image_capture.capture()
+        camera.stop()
+
+        # 处理登录结果
+        # 此处需要根据具体实现方法进行修改
+        if qr_code:
+            self.username.setText('Login Success')
+        else:
+            self.username.setText('Login Failed')
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    login_window = LoginWindow()
+    login_window.show()
+    sys.exit(app.exec_())
