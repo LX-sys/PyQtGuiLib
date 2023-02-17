7ccd9b00bd827166a754918dc909017803870c5d
LX-sys <lx984608061@163.com>
2023-02-18 00:51:35 +0800

    更新

--- PyQtGuiLib/core/__init__.py
+++ PyQtGuiLib/core/__init__.py
@@ -4,6 +4,6 @@ from PyQtGuiLib.core.slideShow import SlideShow
 from PyQtGuiLib.core.flowLayout import FlowLayout
 from PyQtGuiLib.core.listWidget import ListWidget
 from PyQtGuiLib.core.rollWidget import RollWidget
-from PyQtGuiLib.core.palettes.colorPalette import ColorPalette
+from PyQtGuiLib.core.palettes.paletteFrame import PaletteFrame
 from PyQtGuiLib.core.notice.notice import Notice,Notices
 from PyQtGuiLib.core.slider.slider import Slider
