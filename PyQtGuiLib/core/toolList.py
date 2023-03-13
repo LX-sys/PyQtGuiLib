40a811a24d44c3f6cadab065db4dae50d740a84f
LX-sys <lx984608061@163.com>
2023-03-14 00:03:36 +0800

    更新ToolListWidget

--- PyQtGuiLib/core/toolList.py
+++ PyQtGuiLib/core/toolList.py
@@ -4,6 +4,7 @@
 # @file:toolList.py
 # @software:PyCharm
 from PyQtGuiLib.header import (
+    is_mac_sys,
     QWidget,
     QVBoxLayout,
     QPushButton,
@@ -13,6 +14,8 @@ from PyQtGuiLib.header import (
     QSizePolicy,
     qt
 )
+
+from PyQtGuiLib.styles import QssStyleAnalysis
 
 
 class ToolListItem(QVBoxLayout):
@@ -33,6 +36,11 @@ class ToolListItem(QVBoxLayout):
 
         self.addWidget(self.__item_btn)
         self.addWidget(self.__item_widget)
+
+        # qss
+        self.__btnQss = QssStyleAnalysis(self.__item_btn)
+        self.__widgetQss = QssStyleAnalysis(self.__item_widget)
+
 
         if text:
             self.setText(text)
@@ -59,22 +67,27 @@ class ToolListItem(QVBoxLayout):
         return self.__item_btn.text()
 
     def defaultStyle(self):
-        self.itemBtn().setStyleSheet('''
-        #itemBtn{
-        background-color: rgb(12, 11, 15);
-        color: rgb(240, 240, 240);
-        font: 12pt "华文楷体";
-        }
-        #itemBtn:hover{
-        font: 13pt "华文楷体";
-        color: rgb(255, 255, 127);
-        }
-        ''')
-        self.itemWidget().setStyleSheet('''
+        self.__btnQss.setQSS('''
+            #itemBtn{
+                background-color: rgb(12, 11, 15);
+                color: rgb(240, 240, 240);
+                font: 12pt "华文楷体";
+            }
+            #itemBtn:hover{
+                font: 13pt "华文楷体";
+                color: rgb(255, 255, 127);
+            }
+                ''')
+
+        self.__widgetQss.setQSS('''
         #itemWidget{
         border:1px solid rgb(12, 11, 15);
         }
         ''')
+
+        if is_mac_sys:
+            self.__btnQss.selector("#itemBtn").updateAttr("font",'14pt "Heiti SC"')
+            self.__btnQss.selector("#itemBtn:hover").updateAttr("font",'16pt "Heiti SC"')
 
     def clear(self):
         self.itemBtn().disconnect()
@@ -96,8 +109,8 @@ class ToolListWidget(QWidget):
         self.resize(250,350)
         self.setObjectName("ToolListWidget")
         self.__core_vboy = QVBoxLayout(self)
-        self.__core_vboy.setSpacing(1)
-        self.__core_vboy.setContentsMargins(2,1,2,1)
+        self.__core_vboy.setSpacing(0)
+        self.__core_vboy.setContentsMargins(0,0,0,0)
 
         self.verSpacer = QSpacerItem(0, 0, QSizePolicy.Minimum, QSizePolicy.Expanding)
 
