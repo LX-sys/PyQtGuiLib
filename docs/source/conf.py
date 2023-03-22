4868b2010965344c77c25b55db16fe776aac1d6c
LX-sys <lx984608061@163.com>
2023-03-22 17:51:58 +0800

    测试docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@ release = '2.4.18.11'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst_parser"]
+extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = []
