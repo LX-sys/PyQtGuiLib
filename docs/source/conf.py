6cea6f455e98b32dc3dec3fc9fc23778b9a0d2e9
LX-sys <lx984608061@163.com>
2023-03-22 18:13:22 +0800

    测试docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,35 +5,19 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-import sys
-from os.path import abspath,dirname
-
-sys.path.insert(0,dirname(dirname(abspath(__file__))))
+import sphinx_rtd_theme
 
 project = 'PyQtGuiLib'
 copyright = '2023, LX'
 author = 'LX'
 release = '2.4.18.11'
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
-templates_path = ['_templates']
-exclude_patterns = []
-
-language = 'zh_CN'
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'bizstyle'
-html_static_path = ['_static']
-
-
-source_parsers = {'.md': 'recommonmark.parser.CommonMarkParser'}
-source_suffix = {
-    '.rst': 'restructuredtext',
-    '.md': 'markdown',
+from recommonmark.parser import CommonMarkParser
+source_parsers = {
+    '.md': CommonMarkParser,
 }
+source_suffix = ['.rst', '.md']
