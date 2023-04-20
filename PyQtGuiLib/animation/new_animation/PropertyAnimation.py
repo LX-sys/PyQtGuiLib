1413d1e6fecbcf162b627bac557be9c6d586c1b5
LX-sys <lx984608061@163.com>
2023-04-20 12:02:36 +0800

    更新新的动画框架

--- PyQtGuiLib/animation/new_animation/PropertyAnimation.py
+++ PyQtGuiLib/animation/new_animation/PropertyAnimation.py
@@ -8,13 +8,32 @@ from PyQtGuiLib.header import (
     QParallelAnimationGroup,
     QSequentialAnimationGroup,
     QObject,
-    QColor
+    QColor,
+    QSize,
+    QPoint
 )
 import re
 import typing
 from typing import Any,List,TypeVar,Tuple,Union
 
 from PyQtGuiLib.styles import QssStyleAnalysis
+
+
+def strToObj(v_str:str)->Union[QColor,Any]:
+    v_str = v_str.lower().strip()
+
+    if "rgb" in v_str or "rgba" in v_str:
+        color_list = [int(i) for i in re.findall(r"[0-9]{1,3}", v_str)]
+        return QColor(*color_list)
+    elif "#" == v_str[0]:
+        return QColor(v_str)
+    elif "px" in v_str or "pt" in v_str:
+        px_number = re.findall("^([0-9]*)px|^([0-9]*)pt",v_str)
+        if px_number:
+            if px_number[0][0]:return int(px_number[0][0])
+            else:return int(px_number[0][1])
+
+
 
 # 单个动画类
 class PropertyAnimation(QPropertyAnimation):
@@ -42,25 +61,36 @@ class PropertyAnimation(QPropertyAnimation):
                     self.setKeyValueAt(step, value)
         self.setEndValue(ev)
 
-    def setSelector(self,selector:str):
+    # 在设置选择器的同时创建出qss对象
+    def setSelector(self,selector:str,qssObj:QssStyleAnalysis=None):
         self.__selector = selector
+        # ----在这里判断是否为QSS属性,如果_qss对象创建过,则直接使用已创建的
+        if qssObj is None and self.__isQssPropertyName(self.propertyName()):
+            self._qss = QssStyleAnalysis(self.targetObject())
+            self._qss.setQSS(self.targetObject().styleSheet())
+        else:
+            self._qss = qssObj
 
-    def selector(self)->str:
+    # 该方法用来检测_qss是否被创建过,存在则返回
+    def qss(self) -> Union[QssStyleAnalysis,bool]:
+        if hasattr(self,"_qss"):
+            return self._qss
+        return False
+
+    def selector(self) -> str:
         return self.__selector
 
     # 判断 propertyName 是否为 QSS的属性
     def __isQssPropertyName(self,propertyName:bytes)->bool:
-        propertyName = propertyName.decode()
-        return True
+        propertyName = propertyName.data().decode()
+        if self.targetObject() and self.selector():
+            if propertyName not in ["pos","size","geometry","windowOpacity"]:
+                return True
+        return False
 
     def setPropertyName(self, propertyName):
         if isinstance(propertyName,str):
             propertyName = propertyName.encode()
-
-        # ----在这里判断是否为QSS属性
-        if self.__isQssPropertyName(propertyName):
-            self.__qss = QssStyleAnalysis(self.targetObject())
-            self.__qss.setQSS(self.targetObject().styleSheet())
         super().setPropertyName(propertyName)
 
     # 函数执行过程中调用的函数
@@ -72,21 +102,31 @@ class PropertyAnimation(QPropertyAnimation):
 
     # 分析this处理返回
     def __analysisThis(self, value: str):
-        if isinstance(value, str) and value.lower().strip() == "this":
-            pass
-        else:
+        '''
+            对 this 的处理,为了保证this能正确的被处理,请确保设置过动画对象
+        '''
+        if not self.targetObject():
             return value
+
+        if self.propertyName() == b"size":
+            return self.targetObject().size()
+        elif self.propertyName() == b"pos":
+            return self.targetObject().pos()
+        elif self.propertyName() == b"geometry":
+            return self.targetObject().rect()
+        elif self.propertyName() == b"windowOpacity":
+            return self.targetObject().windowOpacity()
 
     # 处理QSS属性动画
     def __selectorAni(self,Any):
-        selector = self.__qss.selector(self.selector())
+        selector = self._qss.selector(self.selector())
         propertyName = self.propertyName().data().decode()
         if selector.isAttr(propertyName):
             if isinstance(Any, int):
                 value = "{}px".format(Any)
             elif isinstance(Any, QColor):
                 value = "rgba({},{},{},{})".format(*Any.getRgb())
-            self.__qss.selector(self.selector()).updateAttr(propertyName, value)
+            self._qss.selector(self.selector()).updateAttr(propertyName, value)
 
     def updateCurrentValue(self, Any):
         if self.func():
@@ -97,7 +137,16 @@ class PropertyAnimation(QPropertyAnimation):
             super().updateCurrentValue(Any)
 
     def setStartValue(self, value: typing.Any) -> None:
-        super().setStartValue(self.__analysisThis(value))
+        if isinstance(value, str) and value.lower().strip() == "this":
+            value = self.__analysisThis(value)
+        elif isinstance(value,str):
+            value = strToObj(value)
+        super().setStartValue(value)
+
+    def setEndValue(self, value: typing.Any) -> None:
+        if isinstance(value, str):
+            value = strToObj(value)
+        super().setEndValue(value)
 
     # 动画执行完成的回调函数
     def finishedCall(self,call):
@@ -106,10 +155,25 @@ class PropertyAnimation(QPropertyAnimation):
     def valueChanged(self, Any):
         super().valueChanged(Any)
 
+# ===================================================
+
 
 class ParallelAnimationGroup(QParallelAnimationGroup):
     def __init__(self,*args,**kwargs):
         super().__init__(*args,**kwargs)
+
+    def build(self,ani):
+        if isinstance(ani.startMode(),ParallelAnimationGroup):
+            group = QParallelAnimationGroup()
+        else:
+            group = QSequentialAnimationGroup()
+        for ani in ani.allAnins():
+            group.addAnimation(ani)
+        self.addAnimation(group)
+
+    def builds(self,anis:list):
+        for ani in anis:
+            self.build(ani)
 
     def addAnimations(self,anis:List[PropertyAnimation]):
         for ani in anis:
@@ -123,3 +187,16 @@ class SequentialAnimationGroup(QSequentialAnimationGroup):
     def addAnimations(self,anis:List[PropertyAnimation]):
         for ani in anis:
             self.addAnimation(ani)
+
+    def build(self, ani):
+        if isinstance(ani.startMode(), ParallelAnimationGroup):
+            group = QParallelAnimationGroup()
+        else:
+            group = QSequentialAnimationGroup()
+        for ani in ani.allAnins():
+            group.addAnimation(ani)
+        self.addAnimation(group)
+
+    def builds(self,anis:list):
+        for ani in anis:
+            self.build(ani)
--- PyQtGuiLib/animation/new_animation/animation.py
+++ PyQtGuiLib/animation/new_animation/animation.py
@@ -5,7 +5,8 @@
 # @software:PyCharm
 from PyQtGuiLib.header import (
     PYQT_VERSIONS,
-    QWidget
+    QWidget,
+    QEasingCurve
 )
 from typing import Callable,Any,TypeVar,List,Union,Tuple
 from PyQtGuiLib.animation.new_animation.PropertyAnimation import ParallelAnimationGroup,SequentialAnimationGroup,PropertyAnimation
@@ -22,13 +23,10 @@ class Animation:
     Sequential = 2
 
     def __init__(self,):
-        # self.__paralle = ParallelAnimationGroup()
-        # self.__sequen = SequentialAnimationGroup()
-
         self.__startMode = SequentialAnimationGroup()  # 默认串行
         self.__anis = []  # type:List[PropertyAnimation]
 
-    def __allAnins(self)->List[PropertyAnimation]:
+    def allAnins(self)->List[PropertyAnimation]:
         return self.__anis
 
     def setStartMode(self,mode:AnimationGroup):
@@ -39,7 +37,13 @@ class Animation:
         else:
             raise Exception("No such mode!")
 
-    def startMode(self)->AnimationGroup:
+    def startMode(self) -> int:
+        if isinstance(self.__startMode,SequentialAnimationGroup):
+            return Animation.Sequential
+        else:
+            return Animation.Parallel
+
+    def startModeObj(self)->AnimationGroup:
         return self.__startMode
 
     def addAni(self,
@@ -48,16 +52,42 @@ class Animation:
                targetObj = None,
                duration:int = 1200,
                propertyName:bytes = b"",
+               loopCount:int = 1,
+               easingCurve:QEasingCurve = None,
                selector:str = None,
                courseFunc:Callable = None,
                finishedCall:Callable = None,
                **kwargs):
+        '''
+
+
+        :param sv:
+        :param ev:
+        :param atv:
+        :param targetObj:
+        :param duration:
+        :param propertyName:
+        :param loopCount:
+        :param easingCurve:
+        :param selector: QSS选择器,注意:如果不是为了样式的动画,请不要设置该属性,避免多余的内存开销
+        :param courseFunc:
+        :param finishedCall:
+        :param kwargs:
+        :return:
+        '''
         if targetObj and propertyName:
             ani = PropertyAnimation()
             ani.setTargetObject(targetObj)
-            ani.setPropertyName(propertyName)
             if selector:
-                ani.setSelector(selector)
+                # 这一段保证了,多个QSS被修改时,最终的结果是一致的
+                obj = self.findTargetObject(targetObj)
+                if obj:
+                    qss = obj.qss()
+                    if qss:ani.setSelector(selector,qss)
+                    else:ani.setSelector(selector)
+                else:
+                    ani.setSelector(selector)
+            ani.setPropertyName(propertyName)
         elif targetObj and courseFunc:
             ani = PropertyAnimation()
             ani.setTargetObject(targetObj)
@@ -78,17 +108,80 @@ class Animation:
         if atv:ani.setStartEndValue(sv,ev,atv)
         else:ani.setStartEndValue(sv, ev)
 
+        if easingCurve:
+            ani.setEasingCurve(easingCurve)
+
         ani.setDuration(duration)
+        ani.setLoopCount(loopCount)
         if finishedCall:
             ani.finishedCall(finishedCall)
         self.__anis.append(ani)
 
+    def addSeriesAni(self,
+                     sv: Any, ev: Any,evs:List[Any],
+                     atv: Union[List[Any], List[Tuple[float, Any]]] = None,
+                     targetObj = None,
+                     duration: int = 1200,
+                     propertyName: bytes = b"",
+                     loopCount: int = 1,
+                     easingCurve: QEasingCurve = None,
+                     selector: str = None,
+                     courseFunc: Callable = None,
+                     finishedCall: Callable = None,
+                     **kwargs):
+        '''
+
+            使用该该函数必须将使用 setStartMode()将动画模式设置为串型(默认也是串型)
+
+        :param sv:
+        :param ev:
+        :param evs: 连续变化值列表(必须是同一类型)
+        :param atv:
+        :param targetObj:
+        :param duration:
+        :param propertyName:
+        :param loopCount:
+        :param easingCurve:
+        :param selector:
+        :param courseFunc:
+        :param finishedCall:
+        :param kwargs:
+        :return:
+        '''
+        if self.startMode() != Animation.Sequential:
+            raise Exception("Sequential Animation is \"Animation.Sequential\" mode.")
+
+        if not evs:
+            raise Exception("The \"evs\" continuous change item should not be empty!")
+
+        # 首先创建第一个动画
+        self.addAni(sv,ev,atv,targetObj,duration,propertyName,
+                    loopCount,easingCurve,selector,courseFunc,None,**kwargs)
+
+        first_value = ev
+        two_value = None
+
+        for _ev in evs[:-1]:
+            two_value = _ev
+            self.addAni(first_value, two_value, atv, targetObj, duration, propertyName,
+                        loopCount, easingCurve, selector, courseFunc, None, **kwargs)
+            first_value = two_value
+
+        if finishedCall:
+            self.addAni(evs[-2], evs[-1],atv,targetObj,duration,propertyName,
+                        loopCount,easingCurve,selector,courseFunc,finishedCall,**kwargs)
+        else:
+            self.addAni(evs[-2], evs[-1], atv, targetObj, duration, propertyName,
+                        loopCount, easingCurve, selector, courseFunc, None, **kwargs)
+
+    # 查找一个控件对象
+    def findTargetObject(self,targetObj:Any)->Union[PropertyAnimation,None]:
+        for obj in self.__anis:
+            if targetObj is obj.targetObject():
+                return obj
+        return None
+
     def start(self):
-        group = self.startMode()
-        group.addAnimations(self.__allAnins())
+        group = self.startModeObj()
+        group.addAnimations(self.allAnins())
         group.start()
-
-import re
-
-s = "221px22"
-print()
